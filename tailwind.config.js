61307001501dcb7a7fc627356ab974f3cf039398
vivekkumar2004-coder-code <vivekkrsahu2004@gmail.com>
2024-12-19 22:58:45 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust paths if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
